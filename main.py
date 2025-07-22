3e5411f67cf41a23579f7691d5e1c06d2f9bb2c8
10BS <reue5118@protonmail.com>
2025-07-22 21:15:29 +1000

    +cmd_ocr

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+from aiogram.types import Message
+from aiogram.methods import SendMessage
 
 from cfg_reader import config
 from cmds import GeminiClient
@@ -23,15 +25,36 @@ async def cmd_start(message: types.Message) -> None:
 
 @dp.message(F.text)
 async def cmd_translate(message: types.Message) -> None:
-    lang: str = "English"
+    lang: str = message.from_user.language_code
     input_text: str = message.text
     output_text = gemini.do_task(task="translate", lang=lang, input_text=input_text)
     await message.reply(output_text)
 
+async def cmd_summarize() -> None:
+    pass
+
+@dp.message(F.photo)
+async def cmd_ocr(message: Message) -> None:
+    file = message.photo[-1].file_id
+    lang: str = message.from_user.language_code
+    output_text = gemini.do_task(task="ocr", lang=lang, file=file)
+    await message.reply(output_text)
+
+async def cmd_describe_picture() -> None:
+    pass
+
+async def cmd_speech_to_text() -> None:
+    pass
+
+async def cmd_speech_to_text_sum() -> None:
+    pass
 
 async def main():
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
